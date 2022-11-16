0dae84914ac1ca5f26c43e61b18a317f1b0a7f53
DiegoLeandroBarrios <dieguinfriky@gmail.com>
2022-11-16 00:32:23 -0300

    Actualizacion del ejercicio 3 al 4

--- ActGlobant3/ActGlobant3/Ejr3.cs
+++ ActGlobant3/ActGlobant3/Ejr3.cs
@@ -8,50 +8,74 @@ namespace ActGlobant3
 {
     internal class Ejr3
     {
-        List<int> numeros = new List<int>();
+        List<NuevosDatos> numeros = new List<NuevosDatos>();
+        Random random = new Random();
         public void Generar()
         {
-            Random random = new Random();
 
             for (int i = 0; i < 1000; i++)
             {
-                int numeroAlazar = random.Next(-100, 101);
-
-                numeros.Add(numeroAlazar);//añade los numeros ramdoms a la lista "numeros"
-
+                try
+                {
+                    /*
+                    int numeroAlazar = random.Next(-100, 101);
+                    NuevosDatos obj = new(numeroAlazar);
+                    numeros.Add(obj);//añade los numeros ramdoms a la lista "numeros"
+                    */
+                    numeros.Add(new NuevosDatos(random.Next(-100, 101)));
+                }
+                catch(Exception e) 
+                {
+                    Console.WriteLine(e.Message);
+                }
             }
         }
-
+        /// <summary>
+        /// Cuenta cuantos numero positivos hay
+        /// </summary>
+        /// <returns></returns>
         public int NumerosPositivos() 
         {
             var positivos = (from i in numeros
-                            where i > 0
+                            where i.Numero > 0
                             select i).Count();//si los numeros de la lista son mayores a 0 o sea positivos, nos cuenta cuantos numeros positivos hay.
 
             return positivos;
         }
+        /// <summary>
+        /// Cuenta cuantos numeros 0 hay
+        /// </summary>
+        /// <returns></returns>
         public int NumerosNulos() 
         {
             var nulos = (from i in numeros
-                             where i == 0
+                             where i.Numero == 0
                              select i).Count();// cuenta cuantos numeros 0 hay
             return nulos;
         }
+        /// <summary>
+        /// devuelve la sumatoria de los numeros negativos
+        /// </summary>
+        /// <returns></returns>
         public int SumaNegativa() 
         {
             var negativa = (from i in numeros
-                            where i < 0
-                            select i).Sum();//suma los numeros negativos
+                            where i.Numero < 0
+                            select i.Numero).Sum();//suma los numeros negativos
                                               
             return  negativa;
                             
         }
+        /// <summary>
+        /// Deveulve el numero que mas se repite en la lista
+        /// </summary>
+        /// <returns></returns>
         public int Repeated()
         {
             var groups = numeros.GroupBy(x => x);//ordenar el objeto entero
             var largest = groups.OrderByDescending(x => x.Count()).First();
 
-            return largest.Key;
+            return largest.Key.Numero;
         }
     }
 }
--- ActGlobant3/ActGlobant3/Program.cs
+++ ActGlobant3/ActGlobant3/Program.cs
@@ -7,7 +7,16 @@ Crear una nueva solución que resuelva el ejercicio de pseudocódigo del ingreso
 	* Que devuelva mediante LINQ la cantidad de veces que aparece el número 0.
 	* Que devuelva mediante LINQ la suma de los números negativos
 	* Que devuelva mediante LINQ el número que se aparece más veces.
-Crear una app de consola que cree una instancia de esta clase y llame a cada uno de estos métodos, mostrando en pantalla el resultado de los mismos*/
+Crear una app de consola que cree una instancia de esta clase y llame a cada uno de estos métodos, mostrando en pantalla el resultado de los mismos
+
+ Ejercicio 4. (SP 3)
+Crear una nueva solución bajo la misma premisa que en el ejercicio 3, pero hay que agregar lo siguiente:
+- La colección no puede ser de int ni de ningún tipo base, deben crear e implementar su propio tipo de dato que:
+	* Tenga un constructor que espere el valor numérico generado por el random. Si el número es 0 debe arrojar una excepción.
+	* Una property read only que devuelva true si el número es positivo o false si es negativo. 
+Los métodos implementados en el ejercicio 3 deben ser refactorizados para que funcionen con este nuevo tipo de dato. 
+Crear una app de consola que cree una instancia de esta clase y llame a cada uno de estos métodos, mostrando en pantalla el resultado de los mismos.
+Fijarse de manejar la excepción y mostrar un mensaje amigable al usuario informando que se intentó crear una instancia con valor = 0.*/
 using ActGlobant3;
 
 Ejr3 ejercicio1 = new Ejr3();
